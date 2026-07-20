92f721fab48776a19b534a0e6d766fb26273de39
Jonathon Fitch <jon.fitch@atomcimedia.co.uk>
2026-07-21 00:39:22 +0100

    Add patch metadata generation for Morphe app compatibility
    - Generate patches-list.json with all 13 patches
    - Generate patches-bundle.json with bundle metadata
    - Include JSON files in .mpp bundle
    - Fixes 'Unnamed could not be updated' error

--- mpp-core/build.gradle.kts
+++ mpp-core/build.gradle.kts
@@ -35,7 +35,139 @@ dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test:2.2.21")
 }
 
+tasks.register("generatePatchMetadata") {
+    val metadataDir = layout.buildDirectory.dir("generated-metadata")
+    outputs.dir(metadataDir)
+
+    doLast {
+        val dir = metadataDir.get().asFile
+        dir.mkdirs()
+
+        file("$dir/patches-list.json").writeText(
+"""
+{
+  "patches": [
+    {
+      "name": "SettingsSupremePartner",
+      "description": "Patch Settings.isSupremePartner to always return true",
+      "default": true,
+      "class": "com.p335p1.mobile.putong.data.SettingsPatchesKt",
+      "field": "settingsSupremePartnerPatch"
+    },
+    {
+      "name": "SettingsPlatinum",
+      "description": "Patch Settings.isPlatinum to always return true",
+      "default": true,
+      "class": "com.p335p1.mobile.putong.data.SettingsPatchesKt",
+      "field": "settingsPlatinumPatch"
+    },
+    {
+      "name": "SettingsODiamond",
+      "description": "Patch Settings.userIsODiamond to always return true",
+      "default": true,
+      "class": "com.p335p1.mobile.putong.data.SettingsPatchesKt",
+      "field": "settingsODiamondPatch"
+    },
+    {
+      "name": "SettingsDisableAds",
+      "description": "Patch Settings.personalizeAdsSuggest to always return false",
+      "default": true,
+      "class": "com.p335p1.mobile.putong.data.SettingsPatchesKt",
+      "field": "settingsDisableAdsPatch"
+    },
+    {
+      "name": "UserIsVip",
+      "description": "Patch User.isVIP to always return true",
+      "default": true,
+      "class": "com.p335p1.mobile.putong.data.UserPatchesKt",
+      "field": "userIsVipPatch"
+    },
+    {
+      "name": "UserIsSVIP",
+      "description": "Patch User.isSVIP to always return true",
+      "default": true,
+      "class": "com.p335p1.mobile.putong.data.UserPatchesKt",
+      "field": "userIsSvipPatch"
+    },
+    {
+      "name": "UserIsUltraPremium",
+      "description": "Patch User.isUltraPremium to always return true",
+      "default": true,
+      "class": "com.p335p1.mobile.putong.data.UserPatchesKt",
+      "field": "userIsUltraPremiumPatch"
+    },
+    {
+      "name": "UserIsSupremePartner",
+      "description": "Patch User.isSupremePartner to always return true",
+      "default": true,
+      "class": "com.p335p1.mobile.putong.data.UserPatchesKt",
+      "field": "userIsSupremePartnerPatch"
+    },
+    {
+      "name": "UserIsPlatinum",
+      "description": "Patch User.isPlatinum to always return true",
+      "default": true,
+      "class": "com.p335p1.mobile.putong.data.UserPatchesKt",
+      "field": "userIsPlatinumPatch"
+    },
+    {
+      "name": "UserIsODiamond",
+      "description": "Patch User.isODiamond to always return true",
+      "default": true,
+      "class": "com.p335p1.mobile.putong.data.UserPatchesKt",
+      "field": "userIsODiamondPatch"
+    },
+    {
+      "name": "UserIsMembership",
+      "description": "Patch User.isMembership to always return true",
+      "default": true,
+      "class": "com.p335p1.mobile.putong.data.UserPatchesKt",
+      "field": "userIsMembershipPatch"
+    },
+    {
+      "name": "UserIsMembershipUsed",
+      "description": "Patch User.isMembershipUsed to always return true",
+      "default": true,
+      "class": "com.p335p1.mobile.putong.data.UserPatchesKt",
+      "field": "userIsMembershipUsedPatch"
+    },
+    {
+      "name": "UserIsVipExpired",
+      "description": "Patch User.isVIPExpired to always return false",
+      "default": true,
+      "class": "com.p335p1.mobile.putong.data.UserPatchesKt",
+      "field": "userIsVipExpiredPatch"
+    }
+  ]
+}
+""".trim()
+        )
+
+        file("$dir/patches-bundle.json").writeText(
+"""
+{
+  "name": "Tantan Premium Bypass",
+  "description": "Unlocks premium features in Tantan v7.2.9",
+  "version": "1.0.0",
+  "patcherVersion": "1.6.0",
+  "compatibleWith": [
+    {
+      "name": "Tantan",
+      "packageName": "com.p335p1.mobile.putong",
+      "version": "7.2.9"
+    }
+  ]
+}
+""".trim()
+        )
+    }
+}
+
 tasks.jar {
+    dependsOn("generatePatchMetadata")
+    from(layout.buildDirectory.dir("generated-metadata")) {
+        include("*.json")
+    }
     archiveExtension.set("mpp")
     manifest {
         attributes(
