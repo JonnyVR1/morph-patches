46f03f7449edf61179573071ec6156848700675e
Jonathon Fitch <jon.fitch@atomcimedia.co.uk>
2026-08-07 22:31:27 +0100

    fix: remove IntlTribeSlideEmptyCardView patch that broke tribe tab navigation
    The IntlTribeSlideEmptyCardView.onFinishInflate() was patched to return void,
    preventing view field initialization via qnp.a(this, view). When the tribe tab
    tried to display data, it caused NullPointerException on null view fields,
    preventing tribe cards from navigating when clicked.
    
    This is the same pattern as the previously fixed IntlTribeGroupBanner bug
    (commit 30255976b3).

--- patches/src/main/java/com/p1/mobile/putong/data/UiCleanupPatch.kt
+++ patches/src/main/java/com/p1/mobile/putong/data/UiCleanupPatch.kt
@@ -282,7 +282,6 @@ val uiCleanupPatch = bytecodePatch(
             "Lcom/p1/mobile/putong/live/external/intl/view/widgets/IntlLiveActivitiesEntryUnRead;",
             "Lcom/p1/mobile/putong/live/external/intl/view/widgets/IntlLiveActivitiesEntryOrigin;",
 
-            "Lcom/p1/mobile/putong/core/newui/intltribe/view/IntlTribeSlideEmptyCardView;",
             "Lcom/p1/mobile/putong/core/newui/intlmeet/visitor/IntlMeetVisitorsEmptyItemView;",
         )
         guideViewDescriptors.forEach { descriptor ->
